af67359766abbb78cbf67da4703b7fb4990c612a
ekaterina <yekaterina.bakina@gmail.com>
2020-07-25 16:20:36 +0300

    sizesCalculator

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,22 @@ import java.util.concurrent.ForkJoinPool;
 
 public class Main
 {
-    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
-
     public static void main(String[] args)
     {
-        System.out.println(getHumanReadableSize(240640));
-        System.exit(0);
-
-        System.out.println(getSizeFromHumanReadable("235K"));
-        System.exit(0);
 
         String folderPath = "/Users/ekaterina/Desktop/special";
         File file = new File(folderPath);
+        Node root = new Node(file);
 
         long start = System.currentTimeMillis();
 
         FolderSizeCalculator calculator =
-                new FolderSizeCalculator(file);
+                new FolderSizeCalculator(root);
 
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
+        pool.invoke(calculator);
 
-        System.out.println(size);
+        System.out.println(root.getSize());
 
 
         //System.out.println(getFolderSize(file));
@@ -34,61 +28,5 @@ public class Main
         System.out.println(duration + " ms");
     }
 
-        public static long getFolderSize(File folder)
-        {
-            if(folder.isFile())
-            {
-                return folder.length();
-            }
-
-            long sum = 0;
-            File[] files = folder.listFiles();
-            for(File file : files)
-            {
-                sum += getFolderSize(file);
-            }
-            return sum;
-        }
-
-        // TODO: 24B, 234KB, 36MB, 34Gb, 42TB
-        public static String getHumanReadableSize(long size)
-        {
-            for (int i = 0; i < sizeMultipliers.length; i++)
-            {
-                double value = size / Math.pow(1024, i);
-                if (value < 1024)
-                {
-                    return Math.round(value) + "" + sizeMultipliers[i] + (i > 0 ? "b" : "");
-                }
-            }
-            return "Very big";
-        }
-
-      // TODO: 24B, 234KB, 36MB, 34Gb, 42TB,
-     //       235K => 240640
-        public static long getSizeFromHumanReadable (String size)
-        {
-            HashMap<Character, Integer> char2Multiplier = getMultipliers();
-            char sizeFactor = size
-                    .replaceAll("[0-9\\s+]+","")
-                    .charAt(0);
-            int multiplier = char2Multiplier.get(sizeFactor);
-            long length = multiplier * Long.valueOf(size.replaceAll("[^0-9]", ""));
-            return length;
-        }
-
-        private static HashMap<Character, Integer> getMultipliers()
-        {
-
-            HashMap<Character, Integer> char2Multiplier = new HashMap<>();
-            for (int i = 0; i < sizeMultipliers.length; i++)
-            {
-                char2Multiplier.put(
-                        sizeMultipliers[i],
-                        (int)Math.pow(1024, i)
-                );
-            }
-            return char2Multiplier;
-        }
-    }
+}
 
